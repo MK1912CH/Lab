fc94dddebf7fcef98e273889910d9731307c51e4
Baco CCB <Baco@ccb.edu.gt>
2023-08-31 08:47:24 -0600

    Add info section

--- src/app/page.js
+++ src/app/page.js
@@ -16,10 +16,38 @@ export default function Home() {
             <span>I am</span>
           </span>
           <br />
-          <span>Mikhel Chour Holm</span>
+          <span>Mikhel Chour</span>
         </h1>
       </div>
     </header>
+    <section className={styles.infoSection}>
+      <img
+        src='/imagen-para-website.png'
+        alt='Imagen'
+        className={styles.image}
+        />
+      <div className={styles.infoContainer}>
+        <span className={styles.title + " " + styles.yellowText}>Mikhel</span>
+        <br/>
+        <span className={styles.title}>Chour</span>
+      </div>
+      <div className={styles.list}>
+        <ul>
+          <li>
+            <span className={styles.grayText}>Age: </span> 27
+          </li>
+          <li>
+            <span className={styles.grayText}>Nationality: </span> German
+          </li>
+          <li>
+            <span className={styles.grayText}>Skill set:</span> Project Management and Financial Performance
+          </li>
+          <li>
+            <spam className={styles.grayText}>Lenguages: </spam> English, German
+          </li>
+        </ul>
+      </div>
+    </section>
   </main>
 
   );
